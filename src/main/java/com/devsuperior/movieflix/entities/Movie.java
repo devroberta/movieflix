0bf00f7223226d9686b8eed6141950949c1860d7
Roberta de Siqueira <roberta.siqueira@zallpy.com>
2022-05-18 21:42:34 -0300

    Entities

---
+++ src/main/java/com/devsuperior/movieflix/entities/Movie.java
@@ -0,0 +1,103 @@
+package com.devsuperior.movieflix.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_movie")
+public class Movie implements Serializable {
+  private static final long serialVersionUID = 1L;
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+  private String title;
+  private String subTitle;
+  private Integer year;
+  private String imgUrl;
+  private String sysnopsis;
+
+  @OneToMany(mappedBy = "movie")
+  private List<Review> events = new ArrayList<>();
+
+  public Movie() {
+  }
+
+  public Movie(Long id, String title, String subTitle, Integer year, String imgUrl, String sysnopsis) {
+    this.id = id;
+    this.title = title;
+    this.subTitle = subTitle;
+    this.year = year;
+    this.imgUrl = imgUrl;
+    this.sysnopsis = sysnopsis;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public void setTitle(String title) {
+    this.title = title;
+  }
+
+  public String getSubTitle() {
+    return subTitle;
+  }
+
+  public void setSubTitle(String subTitle) {
+    this.subTitle = subTitle;
+  }
+
+  public Integer getYear() {
+    return year;
+  }
+
+  public void setYear(Integer year) {
+    this.year = year;
+  }
+
+  public String getImgUrl() {
+    return imgUrl;
+  }
+
+  public void setImgUrl(String imgUrl) {
+    this.imgUrl = imgUrl;
+  }
+
+  public String getSysnopsis() {
+    return sysnopsis;
+  }
+
+  public void setSysnopsis(String sysnopsis) {
+    this.sysnopsis = sysnopsis;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    Movie movie = (Movie) o;
+    return Objects.equals(id, movie.id);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(id);
+  }
+}
---
+++ src/main/java/com/devsuperior/movieflix/entities/Review.java
@@ -0,0 +1,68 @@
+package com.devsuperior.movieflix.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_review")
+public class Review implements Serializable {
+  private static final long serialVersionUID = 1L;
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+  private String text;
+
+  @ManyToOne
+  @JoinColumn(name = "movie_id")
+  private Movie movie;
+
+  @ManyToOne
+  @JoinColumn(name="review_id")
+  private User user;
+
+  public Review() {
+  }
+
+  public Review(Long id, String text) {
+    this.id = id;
+    this.text = text;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getText() {
+    return text;
+  }
+
+  public void setText(String text) {
+    this.text = text;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    Review review = (Review) o;
+    return Objects.equals(id, review.id);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(id);
+  }
+}
